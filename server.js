63136726dc734a4263c30cf5870732d919567bfd
DEEP-808 <65857633+DEEP-808@users.noreply.github.com>
2021-04-30 10:29:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+ 
+const app = express();
+
+app.use(express.static('frontend'));
+
+app.get("/", function(req, res){
+    res.send("Welcome to My Basic Site");
+})
+ 
+app.get("/resume", function(req, res){
+    let path = __dirname+"/frontend/html/page.html";
+    res.sendFile(path);
+})
+// Heroku will automatically set an environment variable called PORT
+const PORT = process.env.PORT || 3000;
+ 
+// Start the server
+app.listen(PORT, function(){
+    console.log("Server Starting running on http://localhost:"+PORT);
+})
